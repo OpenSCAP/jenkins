ee3056d7e482fc0637f422cf8197026fd17c2ba6
Alexander Scheel <ascheel@redhat.com>
2018-08-13 16:17:49 -0400

    Update letsencrypt.sh to support more frequent renewals
    Signed-off-by: Alexander Scheel <ascheel@redhat.com>

--- ansible/master_cfg/letsencrypt.sh
+++ ansible/master_cfg/letsencrypt.sh
@@ -1,9 +1,22 @@
 #!/bin/bash
 
-# TODO: Change the email to something that makes more sense
-/root/letsencrypt/letsencrypt-auto certonly --webroot -w /letsencrypt_public_html/ -d jenkins.open-scap.org -m mpreisle@redhat.com --agree-tos -n --renew-by-default
+# Updates on 2018-08-13 by ascheel to gracefully handle autorenewal
+# Add --keep-until-expiring, --post-hook "systemctl restart nginx" to letsencrypt-auto
+# Remove --renew-by-default aka --force-renewal from letsencrypt-auto
+# Made /etc/nginx/tls/server.{crt,key} symlinks to /etc/letsencrypt/live/jenkins.open-scap.org/{fullchain.pem,privkey.pem}
+# Updated crontab to execute every day, not once a month
+# Added logging of command output to /var/log/letsencrypt-renewal.log
+# Logs for renewal failures are in /var/logs/letsencrypt/letsencrypt.log* and are timestamped
 
-# deploy to nginx
-cp -L /etc/letsencrypt/live/jenkins.open-scap.org/fullchain.pem /etc/nginx/tls/server.crt
-cp -L /etc/letsencrypt/live/jenkins.open-scap.org/privkey.pem /etc/nginx/tls/server.key
-systemctl restart nginx
+log="/var/log/letsencrypt-renewal.log"
+
+echo "=====BEGIN RENEWAL $(date)=====" >> $log
+
+# TODO: Change the email to something that makes more sense
+/root/letsencrypt/letsencrypt-auto certonly --keep-until-expiring --webroot -w /letsencrypt_public_html/ -d jenkins.open-scap.org -m mpreisle@redhat.com --agree-tos -n --post-hook "systemctl restart nginx" >> $log 2>&1
+
+ret=$?
+echo "letsencrypt-auto return code: $ret" >> $log 2>&1
+echo "=====END RENEWAL=====" >> $log
+exit $ret
+
