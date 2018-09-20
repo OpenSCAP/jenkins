1dd4896b01a79947228483896b455026bbe85bb9
Matěj Týč <matyc@redhat.com>
2018-09-20 11:18:05 +0200

    Parametrized the masters domain.

--- ansible/master_cfg/letsencrypt.sh
+++ ansible/master_cfg/letsencrypt.sh
@@ -1,9 +1,12 @@
 #!/bin/bash
 
-# TODO: Change the email to something that makes more sense
-/root/letsencrypt/letsencrypt-auto certonly --webroot -w /letsencrypt_public_html/ -d jenkins.open-scap.org -m mpreisle@redhat.com --agree-tos -n --renew-by-default
+/root/letsencrypt/letsencrypt-auto certonly --webroot -w /letsencrypt_public_html/
+{%- for domain in domains %}
+ -d {{ domain }}
+{%- endfor %}
+ -m openscap-maint@redhat.com --agree-tos -n --renew-by-default
 
 # deploy to nginx
-cp -L /etc/letsencrypt/live/jenkins.open-scap.org/fullchain.pem /etc/nginx/tls/server.crt
-cp -L /etc/letsencrypt/live/jenkins.open-scap.org/privkey.pem /etc/nginx/tls/server.key
+cp -L /etc/letsencrypt/live/{{ domains[0] }}/fullchain.pem /etc/nginx/tls/server.crt
+cp -L /etc/letsencrypt/live/{{ domains[0] }}/privkey.pem /etc/nginx/tls/server.key
 systemctl restart nginx
